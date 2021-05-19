a928337b8c7502fca8fb6cd675862728947df167
atahiri <atahiri@student.1337.ma>
2021-05-19 18:15:00 +0100

    Contact cpp

--- Module 00/ex01/Contact.class.cpp
+++ Module 00/ex01/Contact.class.cpp
@@ -6,7 +6,7 @@
 /*   By: atahiri <atahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 15:03:45 by atahiri           #+#    #+#             */
-/*   Updated: 2021/05/19 15:11:57 by atahiri          ###   ########.fr       */
+/*   Updated: 2021/05/19 16:51:43 by atahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,4 +43,73 @@ void	Contact::getData() {
 	std::getline(std::cin, this->_underwear_color);
 	std::cout << "Enter darkest secret: ";
 	std::getline(std::cin, this->_darkest_secret);
+}
+
+void	Contact::printData() {
+	std::cout << "First Name: ";
+	std::cout << this->_first_name << std::endl;
+	std::cout << "Last Name: ";
+	std::cout << this->_last_name << std::endl;
+	std::cout << "NickName: ";
+	std::cout << this->_nickname << std::endl;
+	std::cout << "Login: ";
+	std::cout << this->_login << std::endl;
+	std::cout << "Postal Address: ";
+	std::cout << this->_postal_address << std::endl;
+	std::cout << "Email Address: ";
+	std::cout << this->_email_address << std::endl;
+	std::cout << "Phone Number: ";
+	std::cout << this->_phone_number << std::endl;
+	std::cout << "Birthday Date: ";
+	std::cout << this->_birthday_date << std::endl;
+	std::cout << "Favorite Meal: ";
+	std::cout << this->_favorite_meal << std::endl;
+	std::cout << "Underwear Color: ";
+	std::cout << this->_underwear_color << std::endl;
+	std::cout << "Darkest Secret: ";
+	std::cout << this->_darkest_secret << std::endl;
+}
+
+std::string	Contact::getFirstName() {
+	return (this->_first_name);
+}
+
+std::string	Contact::getLastName() {
+	return (this->_last_name);
+}
+
+std::string	Contact::getNickname() {
+	return (this->_nickname);
+}
+
+std::string	Contact::getLogin() {
+	return (this->_login);
+}
+
+std::string	Contact::getPostalAddress() {
+	return (this->_postal_address);
+}
+
+std::string	Contact::getEmailAddress() {
+	return (this->_email_address);
+}
+
+std::string	Contact::getPhoneNumber() {
+	return (this->_phone_number);
+}
+
+std::string	Contact::getBirthdayDate() {
+	return (this->_birthday_date);
+}
+
+std::string	Contact::getFavoriteMeal() {
+	return (this->_favorite_meal);
+}
+
+std::string	Contact::getUnderwearColor() {
+	return (this->_underwear_color);
+}
+
+std::string Contact::getDarkestSecret() {
+	return (this->_darkest_secret);
 }
