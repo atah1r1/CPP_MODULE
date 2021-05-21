6a5fd47d5e2579cfae1a948bd4cef35260e2ccd9
Amine Tahiri <atahiri@student.1337.ma>
2021-05-21 11:38:57 +0100

    Limiting phone book

--- Module 00/ex01/main.cpp
+++ Module 00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: atahiri <atahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 18:29:56 by atahiri           #+#    #+#             */
-/*   Updated: 2021/05/21 10:33:29 by atahiri          ###   ########.fr       */
+/*   Updated: 2021/05/21 11:37:03 by atahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int		main(int argc, char *argv[])
 {
 	std::string buff;
 	int		nb(0);
-	Contact contact[8];
+	Contact contact[3];
 
 	while (1) {
 		std::cout <<  "Enter Command: " ;
@@ -27,12 +27,21 @@ int		main(int argc, char *argv[])
 
 		else if (buff == "ADD") 
 		{
-			contact[nb].setData();
-			nb++;
+			if (nb < 3){
+                contact[nb].setData();
+				nb++;
+			}
+            else
+                std::cout << "\033[1;31mPhonebook is full ...!\033[0m" << std::endl;
 		}
 		else if (buff == "SEARCH") {
-			for (int i = 0; i < nb; i++) {
-				contact[i].printData();
+			if (nb > 0) {
+				for (int i = 0; i < nb; i++) {
+					contact[i].printData();
+				}
+			}
+			else {
+				std::cout << "\033[1;31mNothing to print ... !\033[0m" << std::endl;
 			}
 		}
 		
