47f0a427581f98a838d9e2719383a23e5a77724b
Amine Tahiri <atahiri@student.1337.ma>
2022-03-16 20:45:35 +0100

    m07 finished

--- Module 07/ex02/Array.hpp
+++ Module 07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: atahiri <atahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 11:50:11 by atahiri           #+#    #+#             */
-/*   Updated: 2022/03/16 11:56:57 by atahiri          ###   ########.fr       */
+/*   Updated: 2022/03/16 20:27:12 by atahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,75 @@ class Array
 {
 private:
     T *ptr;
-    int size;
+    int _size;
 
 public:
-    Array()
+    Array<T>() : ptr(NULL), _size(0)
     {
-        std::cout << "Constructor Called" << std::endl;
     }
-    ~Array();
+    Array(unsigned int n) : _size(n)
+    {
+        if (size() <= 0)
+        {
+            throw std::invalid_argument("Enter a size greater than 0");
+            return;
+        }
+        ptr = new T[size()];
+    }
+    ~Array<T>()
+    {
+        if (ptr)
+            delete[] ptr;
+        ptr = NULL;
+    }
+    Array<T>(const Array<T> &obj)
+    {
+        *this = obj;
+    }
+
+    unsigned int size(void) const
+    {
+        return this->_size;
+    }
+
+    Array<T> &operator=(const Array<T> &obj)
+    {
+        if (this->size() == obj.size())
+        {
+            for (int i = 0; i < this->_size; i++)
+                this->ptr[i] = obj.ptr[i];
+        }
+        else
+        {
+            this->_size = obj.size();
+            this->ptr = new T[obj.size()];
+            for (int i = 0; i < this->_size; i++)
+            {
+                this->ptr[i] = obj.ptr[i];
+            }
+        }
+        return *this;
+    }
+
+    T &operator[](int index)
+    {
+        if (index >= this->_size || index < 0)
+        {
+            std::out_of_range("Array index out of bound !");
+            exit(0);
+        }
+        return ptr[index];
+    }
+
+    const T &operator[](int index) const
+    {
+        if (index >= this->_size || index < 0)
+        {
+            std::out_of_range("Array index out of bound !");
+            exit(0);
+        }
+        return ptr[index];
+    }
 };
 
 #endif
--- Module 07/ex02/main.cpp
+++ Module 07/ex02/main.cpp
@@ -6,14 +6,60 @@
 /*   By: atahiri <atahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 11:47:49 by atahiri           #+#    #+#             */
-/*   Updated: 2022/03/16 12:00:03 by atahiri          ###   ########.fr       */
+/*   Updated: 2022/03/16 20:30:50 by atahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
-int main()
+#define MAX_VAL 750
+
+int main(int, char **)
 {
-    Array<int> x;
+    Array<int> numbers(MAX_VAL);
+    int *mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    // //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
     return 0;
 }
