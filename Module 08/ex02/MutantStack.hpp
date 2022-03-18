603768e9c944b4f1108bb4f3a3ff72fcc8e496a6
Amine Tahiri <atahiri@student.1337.ma>
2022-03-18 14:06:00 +0100

    okok

---
+++ Module 08/ex02/MutantStack.hpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atahiri <atahiri@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/18 13:55:41 by atahiri           #+#    #+#             */
+/*   Updated: 2022/03/18 14:05:21 by atahiri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+#include <iterator>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+private:
+    /* data */
+public:
+    MutantStack(){};
+    ~MutantStack(){};
+    MutantStack(const MutantStack &obj)
+    {
+        *this = obj;
+    }
+    MutantStack &operator=(const MutantStack &obj)
+    {
+        this->c = obj.c;
+        return *this;
+    }
+    iterator begin(void)
+    {
+        return this->c.begin();
+    }
+    iterator end(void)
+    {
+        return this->c.end();
+    }
+};
+
+#endif
